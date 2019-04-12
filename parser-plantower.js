82e633276c8d5dabc62be0002e1a1c4020201b4a
Marcin Sielski <marcin.sielski@gmail.com>
2019-04-12 22:49:46 +0200

    Migration to new version of serialport

---
+++ parser-plantower.js
@@ -0,0 +1,73 @@
+'use strict';
+const Transform = require('stream').Transform;
+const debugTrans = require('debug')('PT_PROTO.TRANS');
+
+const HEAD1  = 0x42;
+const HEAD2 = 0x4d;
+
+class PlantowerParser extends Transform {
+
+    constructor(options = {}) {
+
+        super(options);
+        this.buffer = Buffer.alloc(0);
+
+    }
+
+
+    checkData(data) {
+
+        let sum = 0;
+        for (let i = 0; i < data.length - 2; i++) {
+            sum += data[i];
+        }
+        return sum === data[data.length - 1] + 256 * data[data.length - 2];
+
+    }
+
+
+    _transform(chunk, encoding, cb) {
+        
+        debugTrans('RX', chunk);
+
+        this.buffer = Buffer.concat([this.buffer, chunk]);
+        let index = -1;
+        for (let i = 0; i < this.buffer.length - 3; i++) { // HEAD2 + LEN * 2 = 3
+            if (this.buffer[i] === HEAD1 && this.buffer[i + 1] == HEAD2) {
+                index = i;
+                break;
+            }
+        }
+        if (index === -1) {
+            cb();
+            return;
+        }
+        let length = (this.buffer[index + 2] * 256 + this.buffer[index + 3]);
+        debugTrans('length: ' + length);
+        let end = index + 4 + length;
+        if (this.buffer.length < end) {
+            cb();
+            return;
+        }
+        if (this.checkData(this.buffer.slice(index, end))) {
+            // remove header + size, and checksum
+            let ret = this.buffer.slice(index + 4, end - 2);
+            this.emit('data', ret);
+        } else {
+            console.warn('check data failed');
+        }
+        this.buffer = this.buffer.slice(end);
+        cb();
+
+    }
+
+    _flush(cb) {
+
+        this.push(this.buffer);
+        this.buffer = Buffer.alloc(0);
+        cb();
+
+    }
+}
+
+module.exports = PlantowerParser;
--- protocol.js
+++ protocol.js
@@ -1,74 +1,45 @@
+'use strict';
 const SerialPort = require('serialport');
-
+const PlantowerParser = require('./parser-plantower');
 const bluebird = require('bluebird');
-const HEAD1  = 0x42
-const HEAD2 = 0x4d
 const debugTrans = require('debug')('PT_PROTO.TRANS');
 const debug = require('debug')('PT_PROTO');
+
+const HEAD1  = 0x42;
+const HEAD2 = 0x4d;
 
 var Protocol = function (dev) {
     this.defer = null;
     this.busy = true;
 
     this.port = new SerialPort(dev, {
-        baudRate: 9600,
-        parser: (() => {
-            let data = new Buffer(0);
-            return (emitter, buffer) => {
-                debugTrans('RX', buffer);
-                data = Buffer.concat([data, buffer]);
-
-                let index = -1;
-                for (let i = 0; i < data.length - 3; i++) { // HEAD2 + LEN * 2 = 3
-                    if (data[i] === HEAD1 && data[i + 1] == HEAD2) {
-                        index = i;
-                        break;
-                    }
-                }
-
-                if (index === -1) {
-                    return;
-                }
-
-                let length = (data[index + 2] * 256 + data[index + 3]);
-                debugTrans('length: ' + length);
-                let end = index + 4 + length;
-                if (data.length < end) {
-                    return;
-                }
-
-                if (this.checkData(data.slice(index, end))) {
-                    // remove header + size, and checksum
-                    let ret = data.slice(index + 4, end - 2);
-                    emitter.emit('data', ret);
-                } else {
-                    console.warn('check data failed');
-                }
-
-                data = data.slice(end);
-            }
-        })()
+        baudRate: 9600
     });
 
     this.queue = [];
 
     let openDefer = bluebird.defer();
-    this.port.on('open', (err) => {
-        if (err) {
-            openDefer.reject(err);
-        } else {
-            openDefer.resolve();
-        }
+    
+    this.port.on('open', () => {
+        openDefer.resolve();
     });
+    
+    this.port.on('error', (err) => {
+        openDefer.reject(err);
+    });
+    
     this.openPromise = openDefer.promise;
-
-    this.port.on('data', (data) => {
+    
+    const parser = this.port.pipe(new PlantowerParser());
+    parser.on('data', (data) => {
         debug('Get data', data);
         this.queue.forEach((defer) => {
             defer.resolve(data);
         });
         this.queue = [];
     });
+    
+    this.port.open(() => {});
 };
 
 Protocol.prototype.read = function () {
@@ -108,15 +79,6 @@ Protocol.prototype.write = function (data) {
     });
 
     return defer.promise;
-};
-
-Protocol.prototype.checkData = function (data) {
-    let sum = 0;
-    for (let i = 0; i < data.length - 2; i++) {
-        sum += data[i];
-    }
-
-    return sum === data[data.length - 1] + 256 * data[data.length - 2];
 };
 
 module.exports = Protocol;
